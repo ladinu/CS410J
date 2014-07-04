32a42bf05376383fe998c9c08ad57e69b671512d
Ladinu Chandrasinghe <ladinu@gmail.com>
2014-07-04 16:20:01 -0700

    Added Airline and Flight class

---
+++ airline/src/test/java/edu/pdx/cs410J/ladinu/Airline.java
@@ -0,0 +1,32 @@
+package edu.pdx.cs410J.ladinu;
+
+import edu.pdx.cs410J.AbstractAirline;
+import edu.pdx.cs410J.AbstractFlight;
+
+import java.util.Collection;
+
+public class Airline extends AbstractAirline {
+  public Airline() {
+    super();
+  }
+
+  @Override
+  public String getName() {
+    return null;
+  }
+
+  @Override
+  public void addFlight(AbstractFlight abstractFlight) {
+
+  }
+
+  @Override
+  public Collection getFlights() {
+    return null;
+  }
+
+  @Override
+  public String toString() {
+    return super.toString();
+  }
+}
---
+++ airline/src/test/java/edu/pdx/cs410J/ladinu/Flight.java
@@ -0,0 +1,51 @@
+package edu.pdx.cs410J.ladinu;
+
+import edu.pdx.cs410J.AbstractFlight;
+
+import java.util.Date;
+
+public class Flight extends AbstractFlight {
+  public Flight() {
+    super();
+  }
+
+  @Override
+  public int getNumber() {
+    return 0;
+  }
+
+  @Override
+  public String getSource() {
+    return null;
+  }
+
+  @Override
+  public Date getDeparture() {
+    return super.getDeparture();
+  }
+
+  @Override
+  public String getDepartureString() {
+    return null;
+  }
+
+  @Override
+  public String getDestination() {
+    return null;
+  }
+
+  @Override
+  public Date getArrival() {
+    return super.getArrival();
+  }
+
+  @Override
+  public String getArrivalString() {
+    return null;
+  }
+
+  @Override
+  public String toString() {
+    return super.toString();
+  }
+}
