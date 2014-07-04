bf695c677533638b9e1c1eaac216fb1b9b3e9e8d
Ladinu Chandrasinghe <ladinu@gmail.com>
2014-07-03 23:02:43 -0700

    Implemented -README option

--- airline/src/main/java/edu/pdx/cs410J/ladinu/Project1.java
+++ airline/src/main/java/edu/pdx/cs410J/ladinu/Project1.java
@@ -1,18 +1,57 @@
 package edu.pdx.cs410J.ladinu;
 
-import edu.pdx.cs410J.AbstractAirline;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * The main class for the CS410J airline Project
  */
-public class sroject1 {
+public class Project1 {
+
+  public static final String USAGE = "" +
+      "usage: java edu.pdx.cs410J.ladinu.Project1 [options] <args>\n" +
+      "  args are (in this order):\n" +
+      "    name                  The name of the airline\n" +
+      "    flightNumber          The flight number\n" +
+      "    src                   Three-letter code of departure airport\n" +
+      "    departTime            Departure date and time (24-hour time)\n" +
+      "    dest                  Three-letter code of arrival airport\n" +
+      "    arriveTime            Arrival date and time (24-hour time)\n" +
+      "  options are (options may appear in any order):\n" +
+      "    -print                Prints a description of the new flight\n" +
+      "    -README               Prints a README for this project and exits\n" +
+      "  Date and time should be in the format: mm/dd/yyyy hh:mm";
 
   public static void main(String[] args) {
-    Class c = AbstractAirline.class;  // Refer to one of Dave's classes so that we can be sure it is on the classpath
+    List<String> commandLineArgs = Arrays.asList(args);
+    printUsageAndExitWithZeroIfReadmeArgGiven(commandLineArgs);
+    ifNoCommandLineArgumentsExitWithOne(commandLineArgs);
+  }
+
+  private static void ifNoCommandLineArgumentsExitWithOne(List<String> commandLineArgs) {
     System.err.println("Missing command line arguments");
-    for (String arg : args) {
+    for (String arg : commandLineArgs) {
       System.out.println(arg);
     }
+    exitWithOne();
+  }
+
+  private static void printUsageAndExitWithZeroIfReadmeArgGiven(List<String> commandLineArgs) {
+    if (commandLineArgs.contains("-README")) {
+      printUsage();
+      exitWithZero();
+    }
+  }
+
+  private static void printUsage() {
+    System.out.println(USAGE);
+  }
+
+  private static void exitWithZero() {
+    System.exit(0);
+  }
+
+  private static void exitWithOne() {
     System.exit(1);
   }
 
--- airline/src/test/java/edu/pdx/cs410J/ladinu/Project1Test.java
+++ airline/src/test/java/edu/pdx/cs410J/ladinu/Project1Test.java
@@ -1,6 +1,7 @@
 package edu.pdx.cs410J.ladinu;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertTrue;
 import edu.pdx.cs410J.InvokeMainTestCase;
 import static junit.framework.Assert.assertEquals;
@@ -10,12 +11,12 @@ import static junit.framework.Assert.assertEquals;
  */
 public class Project1Test extends InvokeMainTestCase {
 
-    /**
-     * Invokes the main method of {@link Project1} with the given arguments.
-     */
-    private MainMethodResult invokeMain(String... args) {
-        return invokeMain( Project1.class, args );
-    }
+  /**
+   * Invokes the main method of {@link Project1} with the given arguments.
+   */
+  private MainMethodResult invokeMain(String... args) {
+    return invokeMain( Project1.class, args );
+  }
 
   /**
    * Tests that invoking the main method with no arguments issues an error
@@ -23,8 +24,22 @@ public class Project1Test extends InvokeMainTestCase {
   @Test
   public void testNoCommandLineArguments() {
     MainMethodResult result = invokeMain();
-    assertEquals(new Integer(1), result.getExitCode());
+    assertExitCodeIsOne(result);
     assertTrue(result.getErr().contains( "Missing command line arguments" ));
   }
 
+  @Test
+  public void shouldPrintUsageWhenReadmeOptionIsGiven() {
+    MainMethodResult result = invokeMain("-README");
+    assertTrue(result.getOut().contains(Project1.USAGE));
+    assertExitCodeIsZero(result);
+  }
+
+  private void assertExitCodeIsZero(MainMethodResult result) {
+    assertEquals(new Integer(0), result.getExitCode());
+  }
+
+  private void assertExitCodeIsOne(MainMethodResult result) {
+    assertEquals(new Integer(1), result.getExitCode());
+  }
 }
