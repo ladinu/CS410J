b03e1f9c3d2fd919c4a300a0eb0205f8ddeec35f
Ladinu Chandrasinghe <ladinu@gmail.com>
2014-07-06 15:14:33 -0700

    Added a test file for Flight

---
+++ airline/src/test/java/edu/pdx/cs410J/ladinu/FlightTest.java
@@ -0,0 +1,43 @@
+package edu.pdx.cs410J.ladinu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FlightTest {
+
+  @Test
+  public void testGetNumber() throws Exception {
+
+  }
+
+  @Test
+  public void testGetSource() throws Exception {
+
+  }
+
+  @Test
+  public void testGetDeparture() throws Exception {
+
+  }
+
+  @Test
+  public void testGetDepartureString() throws Exception {
+
+  }
+
+  @Test
+  public void testGetDestination() throws Exception {
+
+  }
+
+  @Test
+  public void testGetArrival() throws Exception {
+
+  }
+
+  @Test
+  public void testGetArrivalString() throws Exception {
+
+  }
+}
