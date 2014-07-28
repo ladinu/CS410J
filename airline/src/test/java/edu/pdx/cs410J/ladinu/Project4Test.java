60ee3e8247510f961e66dab21a4cd35613eb3cf6
Ladinu Chandrasinghe <ladinu@gmail.com>
2014-07-27 23:34:19 -0700

    Removed airline

--- airline/src/test/java/edu/pdx/cs410J/ladinu/Project4Test.java
+++
@@ -1,9 +0,0 @@
-package edu.pdx.cs410J.ladinu;
-
-import edu.pdx.cs410J.InvokeMainTestCase;
-
-/**
- * Tests the functionality in the {@link Project4} main class.
- */
-public class Project4Test extends InvokeMainTestCase {
-}
