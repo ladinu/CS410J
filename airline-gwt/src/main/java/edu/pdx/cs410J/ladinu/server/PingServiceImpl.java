8e0e3fa8f63907f0b3b76722b88cd73fc248de62
Ladinu Chandrasinghe <ladinu@gmail.com>
2014-08-01 16:15:03 -0700

    inital commit

---
+++ airline-gwt/src/main/java/edu/pdx/cs410J/ladinu/server/PingServiceImpl.java
@@ -0,0 +1,20 @@
+package edu.pdx.cs410J.ladinu.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import edu.pdx.cs410J.AbstractAirline;
+import edu.pdx.cs410J.ladinu.client.Airline;
+import edu.pdx.cs410J.ladinu.client.Flight;
+import edu.pdx.cs410J.ladinu.client.PingService;
+
+/**
+ * The server-side implementation of the Airline service
+ */
+public class PingServiceImpl extends RemoteServiceServlet implements PingService
+{
+    public AbstractAirline ping()
+    {
+        Airline airline = new Airline();
+        airline.addFlight( new Flight() );
+        return airline;
+    }
+}
