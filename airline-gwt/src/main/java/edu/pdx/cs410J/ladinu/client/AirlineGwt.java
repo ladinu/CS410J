19beea5b37e736210483562a675ee46c01620d17
Ladinu Chandrasinghe <ladinu@gmail.com>
2014-08-09 00:55:32 -0700

    Finished html for ui

--- airline-gwt/src/main/java/edu/pdx/cs410J/ladinu/client/AirlineGwt.java
+++ airline-gwt/src/main/java/edu/pdx/cs410J/ladinu/client/AirlineGwt.java
@@ -1,27 +1,35 @@
 package edu.pdx.cs410J.ladinu.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
+
 
 /**
  * A basic GWT class that makes sure that we can send an airline back from the server
  */
 public class AirlineGwt implements EntryPoint {
   public void onModuleLoad() {
-    HorizontalPanel container = new HorizontalPanel();
+    Button button = getButton("flight-add-button");
+    final TextBox textBox = getTextBox("airline-name-text-box");
 
-    Label airlineNameLabel = new Label("Airline Name: ");
-    TextBox airlineNameTextBox = new TextBox();
-    setPlaceHolder(airlineNameTextBox, "Alaska");
 
-    container.add(airlineNameLabel);
-    container.add(airlineNameTextBox);
-
-    RootPanel rootPanel = RootPanel.get();
-    //rootPanel.add(container);
+    button.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent clickEvent) {
+        textBox.setText("foo bar");
+      }
+    });
   }
 
-  private static void setPlaceHolder(Widget widget, String placeHolderText) {
-    widget.getElement().setAttribute("placeholder", placeHolderText);
+  public static Button getButton(String id) {
+    return Button.wrap(Document.get().getElementById(id));
+  }
+
+  public static TextBox getTextBox(String id) {
+    return TextBox.wrap(Document.get().getElementById(id));
   }
 }
